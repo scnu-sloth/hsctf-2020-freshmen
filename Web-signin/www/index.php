b611baa831397277947e2a9167eea92792abb3e6
Fxizenta <1193295835@qq.com>
2021-07-12 11:50:11 +0800

    更新fxizenta的题目

---
+++ Web-signin/www/index.php
@@ -0,0 +1,12 @@
+<?php
+header('flag: hsctf{w3lcome_t0_hsc7f}');
+header('location:./error.php');
+?>
+<html>
+<head><title>404 Not Found</title></head>
+<body bgcolor="white">
+<center><h1>404 Not Found</h1></center>
+<hr><center>nginx</center>
+</body>
+
+</html>
